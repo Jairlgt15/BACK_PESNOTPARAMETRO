0eae52d1f9c90cf5c019ec0a68ea939f725caee2
JG <ludwing.gomez@epn.edu.ec>
2022-10-11 22:05:04 -0500

    Fk pero mandando todo el objeto

--- src/main/java/ec/gob/cj/pesnot/paginaprincipal/catalogoservicios/modelo/Parametro.java
+++ src/main/java/ec/gob/cj/pesnot/paginaprincipal/catalogoservicios/modelo/Parametro.java
@@ -49,27 +49,13 @@ public class Parametro {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	
 
-	public Parametro(Catalogo catalogo, Long numero, String descripcion, Date fecha, Date fechaInicio, Date fechaFin,
-			boolean estadoActivo, boolean estadoParametroCatalogo, String nombreParametro, Long valorParametro) {
-		super();
-		this.catalogo.setId(catalogo.getId());
-		this.numero = numero;
-		this.descripcion = descripcion;
-		this.fecha = fecha;
-		this.fechaInicio = fechaInicio;
-		this.fechaFin = fechaFin;
-		this.estadoActivo = estadoActivo;
-		this.estadoParametroCatalogo = estadoParametroCatalogo;
-		this.nombreParametro = nombreParametro;
-		this.valorParametro = valorParametro;
-	}
-	public Parametro(Long idParametro, Catalogo catalogo, Long numero, String descripcion, Date fecha, Date fechaInicio, Date fechaFin,
-			boolean estadoActivo, boolean estadoParametroCatalogo, String nombreParametro, Long valorParametro) {
+	public Parametro(Long idParametro, Catalogo catalogo, Long numero, String descripcion, Date fecha, Date fechaInicio,
+			Date fechaFin, boolean estadoActivo, boolean estadoParametroCatalogo, String nombreParametro,
+			Long valorParametro) {
 		super();
 		this.idParametro = idParametro;
-		this.catalogo.setId(catalogo.getId());
+		this.catalogo = catalogo;
 		this.numero = numero;
 		this.descripcion = descripcion;
 		this.fecha = fecha;
@@ -80,80 +66,165 @@ public class Parametro {
 		this.nombreParametro = nombreParametro;
 		this.valorParametro = valorParametro;
 	}
+	public Parametro( Catalogo catalogo, Long numero, String descripcion, Date fecha, Date fechaInicio,
+			Date fechaFin, boolean estadoActivo, boolean estadoParametroCatalogo, String nombreParametro,
+			Long valorParametro) {
+		super();
+		this.catalogo = catalogo;
+		this.numero = numero;
+		this.descripcion = descripcion;
+		this.fecha = fecha;
+		this.fechaInicio = fechaInicio;
+		this.fechaFin = fechaFin;
+		this.estadoActivo = estadoActivo;
+		this.estadoParametroCatalogo = estadoParametroCatalogo;
+		this.nombreParametro = nombreParametro;
+		this.valorParametro = valorParametro;
+	}
+
 
 
 	public Long getIdParametro() {
 		return idParametro;
 	}
 
+
+
 	public void setIdParametro(Long idParametro) {
 		this.idParametro = idParametro;
 	}
-	public Long getIdCatalogo() {
-		return catalogo.getId();
+
+
+
+	public Catalogo getCatalogo() {
+		return catalogo;
 	}
-	public void setIdCatalogo(Long idCatalogo) {
-		this.catalogo.setId(idCatalogo);
+
+
+
+	public void setCatalogo(Catalogo catalogo) {
+		this.catalogo = catalogo;
 	}
+
+
+
 	public Long getNumero() {
 		return numero;
 	}
+
+
+
 	public void setNumero(Long numero) {
 		this.numero = numero;
 	}
+
+
+
 	public String getDescripcion() {
 		return descripcion;
 	}
+
+
+
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
+
+
+
 	public Date getFecha() {
 		return fecha;
 	}
+
+
+
 	public void setFecha(Date fecha) {
 		this.fecha = fecha;
 	}
+
+
+
 	public Date getFechaInicio() {
 		return fechaInicio;
 	}
+
+
+
 	public void setFechaInicio(Date fechaInicio) {
 		this.fechaInicio = fechaInicio;
 	}
+
+
+
 	public Date getFechaFin() {
 		return fechaFin;
 	}
+
+
+
 	public void setFechaFin(Date fechaFin) {
 		this.fechaFin = fechaFin;
 	}
+
+
+
 	public boolean isEstadoActivo() {
 		return estadoActivo;
 	}
+
+
+
 	public void setEstadoActivo(boolean estadoActivo) {
 		this.estadoActivo = estadoActivo;
 	}
+
+
+
 	public boolean isEstadoParametroCatalogo() {
 		return estadoParametroCatalogo;
 	}
+
+
+
 	public void setEstadoParametroCatalogo(boolean estadoParametroCatalogo) {
 		this.estadoParametroCatalogo = estadoParametroCatalogo;
 	}
+
+
+
 	public String getNombreParametro() {
 		return nombreParametro;
 	}
+
+
+
 	public void setNombreParametro(String nombreParametro) {
 		this.nombreParametro = nombreParametro;
 	}
+
+
+
 	public Long getValorParametro() {
 		return valorParametro;
 	}
+
+
+
 	public void setValorParametro(Long valorParametro) {
 		this.valorParametro = valorParametro;
 	}
+
+
+
 	@Override
 	public String toString() {
-		return "Parametro [idParametro=" + idParametro + ", idCatalogo=" + catalogo.getId() + ", numero=" + numero
+		return "Parametro [idParametro=" + idParametro + ", catalogo=" + catalogo + ", numero=" + numero
 				+ ", descripcion=" + descripcion + ", fecha=" + fecha + ", fechaInicio=" + fechaInicio + ", fechaFin="
 				+ fechaFin + ", estadoActivo=" + estadoActivo + ", estadoParametroCatalogo=" + estadoParametroCatalogo
-				+ "]";
+				+ ", nombreParametro=" + nombreParametro + ", valorParametro=" + valorParametro + "]";
 	}
+	
+	
+	
+
 }
