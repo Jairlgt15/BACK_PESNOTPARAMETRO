0079b47110174e600b168d7322f458d65074adae
JG <ludwing.gomez@epn.edu.ec>
2022-10-18 11:24:18 -0500

    arreglo de importaciones

--- src/main/java/ec/gob/cj/pesnot/paginaprincipal/catalogoservicios/controlador/CatalogoControlador.java
+++ src/main/java/ec/gob/cj/pesnot/paginaprincipal/catalogoservicios/controlador/CatalogoControlador.java
@@ -1,13 +1,11 @@
 package ec.gob.cj.pesnot.paginaprincipal.catalogoservicios.controlador;
 
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
