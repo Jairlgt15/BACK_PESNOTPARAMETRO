582ed897b7c3a708199c8f79b32f99baadda04d8
JG <ludwing.gomez@epn.edu.ec>
2022-10-24 16:50:41 -0500

    sin fecha

--- src/main/java/ec/gob/cj/pesnot/paginaprincipal/catalogoservicios/modelo/Parametro.java
+++ src/main/java/ec/gob/cj/pesnot/paginaprincipal/catalogoservicios/modelo/Parametro.java
@@ -33,10 +33,6 @@ public class Parametro {
 	private String descripcion;
 	@Column(name="FECHAPARAMETROCATALOGO")
 	private Date fecha;
-	@Column(name="FECHAINICIOPARAMETROCATALOGO")
-	private Date fechaInicio;
-	@Column(name="FECHAFINPARAMETROCATALOGO")
-	private Date fechaFin;
 	@Column(name="ESTADOACTIVO")
 	private boolean estadoActivo;
 	@Column(name="ESTADOPARAMETROCATALOGO")
@@ -56,7 +52,7 @@ public class Parametro {
 
 
 	public Parametro(Long idParametro, Catalogo idCatalogo, Long numero, String descripcion, Date fecha,
-			Date fechaInicio, Date fechaFin, boolean estadoActivo, boolean estadoParametroCatalogo,
+			boolean estadoActivo, boolean estadoParametroCatalogo,
 			String nombreParametro, Long valorParametro) {
 		super();
 		this.idParametro = idParametro;
@@ -64,8 +60,6 @@ public class Parametro {
 		this.numero = numero;
 		this.descripcion = descripcion;
 		this.fecha = fecha;
-		this.fechaInicio = fechaInicio;
-		this.fechaFin = fechaFin;
 		this.estadoActivo = estadoActivo;
 		this.estadoParametroCatalogo = estadoParametroCatalogo;
 		this.nombreParametro = nombreParametro;
@@ -74,15 +68,13 @@ public class Parametro {
 
 
 
-	public Parametro(Catalogo idCatalogo, Long numero, String descripcion, Date fecha, Date fechaInicio, Date fechaFin,
+	public Parametro(Catalogo idCatalogo, Long numero, String descripcion, Date fecha,
 			boolean estadoActivo, boolean estadoParametroCatalogo, String nombreParametro, Long valorParametro) {
 		super();
 		this.idCatalogo = idCatalogo;
 		this.numero = numero;
 		this.descripcion = descripcion;
 		this.fecha = fecha;
-		this.fechaInicio = fechaInicio;
-		this.fechaFin = fechaFin;
 		this.estadoActivo = estadoActivo;
 		this.estadoParametroCatalogo = estadoParametroCatalogo;
 		this.nombreParametro = nombreParametro;
@@ -150,31 +142,6 @@ public class Parametro {
 	}
 
 
-
-	public Date getFechaInicio() {
-		return fechaInicio;
-	}
-
-
-
-	public void setFechaInicio(Date fechaInicio) {
-		this.fechaInicio = fechaInicio;
-	}
-
-
-
-	public Date getFechaFin() {
-		return fechaFin;
-	}
-
-
-
-	public void setFechaFin(Date fechaFin) {
-		this.fechaFin = fechaFin;
-	}
-
-
-
 	public boolean isEstadoActivo() {
 		return estadoActivo;
 	}
@@ -226,8 +193,7 @@ public class Parametro {
 	@Override
 	public String toString() {
 		return "Parametro [idParametro=" + idParametro + ", idcatalogo=" + idCatalogo + ", numero=" + numero
-				+ ", descripcion=" + descripcion + ", fecha=" + fecha + ", fechaInicio=" + fechaInicio + ", fechaFin="
-				+ fechaFin + ", estadoActivo=" + estadoActivo + ", estadoParametroCatalogo=" + estadoParametroCatalogo
+				+ ", descripcion=" + descripcion + ", fecha=" + fecha + ", fechaInicio=" + ", estadoActivo=" + estadoActivo + ", estadoParametroCatalogo=" + estadoParametroCatalogo
 				+ ", nombreParametro=" + nombreParametro + ", valorParametro=" + valorParametro + "]";
 	}
 
--- src/test/java/ec/gob/cj/pesnot/paginaprincipal/catalogoservicios/service/ParametroServiceTest.java
+++ src/test/java/ec/gob/cj/pesnot/paginaprincipal/catalogoservicios/service/ParametroServiceTest.java
@@ -57,8 +57,6 @@ class ParametroServiceTest {
         parametro.setIdCatalogo(catalogo);
         parametro.setDescripcion("a");
         parametro.setFecha(null);
-        parametro.setFechaInicio(null);
-        parametro.setFechaFin(null);
         parametro.setNombreParametro("a");
         parametro.setNumero((long) 1);
         parametro.setValorParametro((long) 1);
@@ -82,17 +80,13 @@ class ParametroServiceTest {
     @DisplayName("Prueba la funcionalidad del modelo")
     void modelo() {
     	Catalogo idCatalogo=new Catalogo();
-    	Parametro parametro1=new Parametro(1l,idCatalogo,1l, "hola", null,null,null,
-    			true,true,"",2l);
-    	Parametro parametro=new Parametro(idCatalogo,1l, "hola", null,null,null,
-    			true,true,"",2l);
+    	Parametro parametro1=new Parametro(1l,idCatalogo,1l, "hola", null,true,true,"",2l);
+    	Parametro parametro=new Parametro(idCatalogo,1l, "hola", null,true,true,"",2l);
     	parametro.setIdParametro(null);
     	parametro.setDescripcion(null);
     	parametro.setEstadoActivo(false);
     	parametro.setEstadoParametroCatalogo(false);
     	parametro.setFecha(null);
-    	parametro.setFechaInicio(null);
-    	parametro.setFechaFin(null);
     	parametro.setNombreParametro(null);
     	parametro.setNumero(null);
     	parametro.setValorParametro(null);
@@ -101,8 +95,6 @@ class ParametroServiceTest {
     	parametro.isEstadoActivo();
     	parametro.isEstadoParametroCatalogo();
     	parametro.getFecha();
-    	parametro.getFechaInicio();
-    	parametro.getFechaFin();
     	parametro.getNombreParametro();
     	parametro.getNumero();
     	parametro.getValorParametro();
@@ -138,10 +130,8 @@ class ParametroServiceTest {
     @DisplayName("Prueba Servicio")
     void servicio() {
     	Catalogo catalogoE= repoC.getById((long) 1);
-    	Parametro parametro1=new Parametro(1l,catalogoE,1l, "hola", null,null,null,
-    			true,true,"",2l);
-    	Parametro parametro=new Parametro(catalogoE,1l, "hola", null,null,null,
-    			true,true,"",2l);
+    	Parametro parametro1=new Parametro(1l,catalogoE,1l, "hola", null,true,true,"",2l);
+    	Parametro parametro=new Parametro(catalogoE,1l, "hola", null,true,true,"",2l);
     	service.actualizar(parametro);
     	service.listarParametro();
     	service.guardarParametro(parametro1);
@@ -154,10 +144,8 @@ class ParametroServiceTest {
     @DisplayName("Prueba controller")
     void controlador() {
     	Catalogo catalogoE= repoC.getById((long) 1);
-    	Parametro parametro1=new Parametro(1l,catalogoE,1l, "hola", null,null,null,
-    			true,true,"",2l);
-    	Parametro parametro=new Parametro(catalogoE,1l, "hola", null,null,null,
-    			true,true,"",2l);
+    	Parametro parametro1=new Parametro(1l,catalogoE,1l, "hola", null,true,true,"",2l);
+    	Parametro parametro=new Parametro(catalogoE,1l, "hola", null,true,true,"",2l);
     	cont.listarParametros();
     	cont.guardarActos(parametro);
     	cont.obtenerParametroById((long) 1);
